3dba3ae09f07dc37df6889cb74d9f6508c1aec6b
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2024-07-04 16:25:09 +0100

    chore(init): configure tailwind and shadcn

--- inertia/pages/home.tsx
+++ inertia/pages/home.tsx
@@ -1,4 +1,5 @@
 import { Head } from '@inertiajs/react'
+import { Button } from '../components/ui/button'
 
 export default function Home(props: { version: number }) {
   return (
@@ -7,7 +8,8 @@ export default function Home(props: { version: number }) {
 
       <div className="container">
         <div className="title">AdonisJS {props.version} x Inertia x React</div>
-
+        <h1 className="text-1xl font-bold text-red-400 underline">Olá mundo Cruel!</h1>
+        <Button variant="outline">Button</Button>
         <span>
           Learn more about AdonisJS and Inertia.js by visiting the{' '}
           <a href="https://docs.adonisjs.com/guides/inertia">AdonisJS documentation</a>.
