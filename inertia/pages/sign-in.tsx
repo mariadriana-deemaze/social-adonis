29619b12c881ef36c03688a1515ef63c6174fcb2
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2024-07-15 22:14:53 +0100

    refactor: use inertia form helper for POST req

--- inertia/pages/sign-in.tsx
+++ inertia/pages/sign-in.tsx
@@ -1,63 +1,76 @@
-import { Head } from '@inertiajs/react'
+import { Head, useForm } from '@inertiajs/react'
 import { Button } from '../components/ui/button'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../components/ui/card'
 import { Input } from '../components/ui/input'
 import { Label } from '../components/ui/label'
 
 export default function SignIn() {
-  const signIn = async () => {
-    const req = await fetch('/auth/sign-in', {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        email: 'testing@gmail.com',
-        password: 'password',
-      }),
+  const { data, setData, post, processing, errors } = useForm({
+    email: '',
+    password: '',
+  })
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault()
+    post('/auth/sign-in', {
+      onSuccess: () => console.log('profit?'),
     })
-
-    const response = await req.json()
-
-    console.log('response ->', response)
   }
 
   return (
     <>
       <Head title="Homepage" />
-      <div className="container gap-4">
-        <Card className="mx-auto max-w-sm">
-          <CardHeader>
-            <CardTitle className="text-xl">Sign in</CardTitle>
-            <CardDescription>Enter your account authentication credentials</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <div className="grid gap-4">
-              <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input id="email" type="email" placeholder="m@example.com" required />
+      <form className="container" onSubmit={handleSubmit}>
+        {processing && 'Hold up!'}
+        {errors && JSON.stringify(errors)}
+        <div className="container gap-4">
+          <Card className="mx-auto max-w-sm">
+            <CardHeader>
+              <CardTitle className="text-xl">Sign in</CardTitle>
+              <CardDescription>Enter your account authentication credentials</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="grid gap-4">
+                <div className="grid gap-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="m@example.com"
+                    value={data.email}
+                    //@ts-ignore
+                    onChange={(e) => setData('email', e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <Label htmlFor="password">Password</Label>
+                  <Input
+                    id="password"
+                    type="password"
+                    value={data.password}
+                    //@ts-ignore
+                    onChange={(e) => setData('password', e.target.value)}
+                  />
+                </div>
+                <Button type="submit" className="w-full">
+                  Login
+                </Button>
               </div>
-              <div className="grid gap-2">
-                <Label htmlFor="password">Password</Label>
-                <Input id="password" type="password" />
+              <div className="mt-4 text-center text-sm">
+                Don't yet have an account?{' '}
+                <a href="/auth/sign-up" className="underline">
+                  Sign up
+                </a>
               </div>
-              <Button onClick={signIn} className="w-full">
-                Login
-              </Button>
-            </div>
-            <div className="mt-4 text-center text-sm">
-              Don't yet have an account?{' '}
-              <a href="/auth/sign-up" className="underline">
-                Sign up
-              </a>
-            </div>
-          </CardContent>
-        </Card>
+            </CardContent>
+          </Card>
 
-        <a href="/posts" className="underline">
-          Try to get posts
-        </a>
-      </div>
+          <a href="/posts" className="underline">
+            Try to get posts
+          </a>
+        </div>
+      </form>
     </>
   )
 }
--- inertia/pages/sign-up.tsx
+++ inertia/pages/sign-up.tsx
@@ -1,67 +1,88 @@
-import { Head } from '@inertiajs/react'
+import { Head, useForm } from '@inertiajs/react'
 import { Button } from '../components/ui/button'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../components/ui/card'
 import { Input } from '../components/ui/input'
 import { Label } from '../components/ui/label'
 
 export default function SignUp() {
-  const signUp = async () => {
-    const req = await fetch('/auth/sign-up', {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        fullName: 'Testing',
-        email: 'testing@gmail.com',
-        password: 'password',
-      }),
+  const { data, setData, post, processing, errors } = useForm({
+    fullName: '',
+    email: '',
+    password: '',
+  })
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault()
+    post('/auth/sign-up', {
+      onSuccess: () => console.log('profit?'),
     })
-
-    const response = await req.json()
-
-    console.log('response ->', response)
   }
 
   return (
     <>
       <Head title="Homepage" />
-      <div className="container gap-4">
-        <Card className="mx-auto max-w-sm">
-          <CardHeader>
-            <CardTitle className="text-xl">Sign Up</CardTitle>
-            <CardDescription>Enter your information to create an account</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <div className="grid gap-4">
-              <div className="grid gap-2">
-                <Label htmlFor="fullname">Full name</Label>
-                <Input id="fullname" type="text" placeholder="John Doe" required />
+      <form className="container" onSubmit={handleSubmit}>
+        {processing && 'Hold up!'}
+        {errors && JSON.stringify(errors)}
+        <div className="container gap-4">
+          <Card className="mx-auto max-w-sm">
+            <CardHeader>
+              <CardTitle className="text-xl">Sign Up</CardTitle>
+              <CardDescription>Enter your information to create an account</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="grid gap-4">
+                <div className="grid gap-2">
+                  <Label htmlFor="fullName">Full name</Label>
+                  <Input
+                    id="fullName"
+                    type="text"
+                    placeholder="John Doe"
+                    value={data.fullName}
+                    //@ts-ignore
+                    onChange={(e) => setData('fullName', e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="m@example.com"
+                    value={data.email}
+                    //@ts-ignore
+                    onChange={(e) => setData('email', e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <Label htmlFor="password">Password</Label>
+                  <Input
+                    id="password"
+                    type="password"
+                    value={data.password}
+                    //@ts-ignore
+                    onChange={(e) => setData('password', e.target.value)}
+                  />
+                </div>
+                <Button type="submit" className="w-full">
+                  Create an account
+                </Button>
               </div>
-              <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input id="email" type="email" placeholder="m@example.com" required />
+              <div className="mt-4 text-center text-sm">
+                Already have an account?{' '}
+                <a href="/auth/sign-in" className="underline">
+                  Sign in
+                </a>
               </div>
-              <div className="grid gap-2">
-                <Label htmlFor="password">Password</Label>
-                <Input id="password" type="password" />
-              </div>
-              <Button onClick={signUp} className="w-full">
-                Create an account
-              </Button>
-            </div>
-            <div className="mt-4 text-center text-sm">
-              Already have an account?{' '}
-              <a href="/auth/sign-in" className="underline">
-                Sign in
-              </a>
-            </div>
-          </CardContent>
-        </Card>
-        <a href="/posts" className="underline">
-          Try to get posts
-        </a>
-      </div>
+            </CardContent>
+          </Card>
+          <a href="/posts" className="underline">
+            Try to get posts
+          </a>
+        </div>
+      </form>
     </>
   )
 }
