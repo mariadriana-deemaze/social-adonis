24835e94462fb49504246231c910d8bd0b85b669
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2024-09-06 01:20:33 +0100

    refactor: review initial scaffolded user account migrations

--- app/models/user.ts
+++ app/models/user.ts
@@ -1,24 +1,43 @@
 import { DateTime } from 'luxon'
 import hash from '@adonisjs/core/services/hash'
 import { compose } from '@adonisjs/core/helpers'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, column, computed } from '@adonisjs/lucid/orm'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
 import { DbAccessTokensProvider } from '@adonisjs/auth/access_tokens'
+// import { randomUUID } from 'crypto'
+import type { UUID } from 'crypto'
+
+export enum AccountRole {
+  USER = 'USER',
+  ADMIN = 'ADMIN',
+}
 
 const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
   uids: ['email'],
   passwordColumnName: 'password',
 })
 
+
 export default class User extends compose(BaseModel, AuthFinder) {
   @column({ isPrimary: true })
-  declare id: number
+  declare id: UUID // = randomUUID()
 
   @column()
-  declare fullName: string | null
+  declare name: string | null
+
+  @column()
+  declare surname: string | null
 
   @column()
   declare email: string
+
+  @column()
+  role: AccountRole = AccountRole.USER
+
+  @computed()
+  get isAdmin() {
+    return this.role === AccountRole.ADMIN
+  }
 
   @column({ serializeAs: null })
   declare password: string
--- database/migrations/1720100420470_create_users_table.ts
+++ database/migrations/1720100420470_create_users_table.ts
@@ -1,15 +1,28 @@
+import { AccountRole } from '#models/user'
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
   protected tableName = 'users'
 
   async up() {
-    this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').notNullable()
-      table.string('full_name').nullable()
-      table.string('email', 254).notNullable().unique()
-      table.string('password').notNullable()
+    await this.db.rawQuery('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
+      .knexQuery
 
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').primary().defaultTo(this.db.rawQuery('uuid_generate_v4()').knexQuery)
+      table.string('name').nullable()
+      table.string('surname').nullable()
+      table.string('email', 254).notNullable().unique()
+      table
+        .enum('role', Object.values(AccountRole))
+        .defaultTo(AccountRole.USER)
+        .notNullable()
+        .comment(
+          ` Role of the user account within the platform. 
+            Registration is for now done the same way, and to attribute an higher role rank 
+            is done directly on the DB - at least for the time being.`
+        )
+      table.string('password').notNullable()
       table.timestamp('created_at').notNullable()
       table.timestamp('updated_at').nullable()
     })
