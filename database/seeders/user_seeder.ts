8c780d09b09ae9fbca180faef8c808cabc5a4e17
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2024-09-07 01:04:58 +0100

    fix: correction to seeder double hashing

--- database/seeders/user_seeder.ts
+++ database/seeders/user_seeder.ts
@@ -1,6 +1,5 @@
 import User, { AccountRole } from '#models/user'
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
-import hash from '@adonisjs/core/services/hash'
 import { faker } from '@faker-js/faker/locale/en_GB'
 
 export default class extends BaseSeeder {
@@ -17,15 +16,10 @@ export default class extends BaseSeeder {
     const admin = {
       name: 'Admin user',
       email: 'admin_user@gmail.com',
-      password: await hash.make('take1WildGuess!'),
+      password: 'take1WildGuess!',
       role: AccountRole.ADMIN
     }
 
-    let data = [...users, admin]
-    for (const user of data) {
-      user.password = await hash.make(user.password)
-    }
-
-    await User.createMany(data)
+    await User.createMany([...users, admin])
   }
 }
