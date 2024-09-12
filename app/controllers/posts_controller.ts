d0366f24e9c431ee820c1d03d53ca8bb1f38006b
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2024-09-12 22:36:53 +0100

    feat: add post policy with bouncer

--- app/controllers/posts_controller.ts
+++ app/controllers/posts_controller.ts
@@ -1,17 +1,23 @@
 import { inject } from '@adonisjs/core'
-import PostService from '#services/post_service'
-import { createPostValidator } from '#validators/post'
 import type { HttpContext } from '@adonisjs/core/http'
 import { errors } from '@vinejs/vine'
+import PostService from '#services/post_service'
+import { createPostValidator } from '#validators/post'
+import PostPolicy from '#policies/post_policy'
+import Post from '#models/post'
 
 @inject()
 export default class PostsController {
   constructor(private postService: PostService) {}
 
   async create(ctx: HttpContext) {
+    if (await ctx.bouncer.with(PostPolicy).denies('create')) {
+      return ctx.response.forbidden('Cannot create a post.')
+    }
+
     try {
       const payload = await ctx.request.validateUsing(createPostValidator)
-      
+
       await this.postService.create({
         content: payload.content,
         userId: ctx.auth.user?.id!,
@@ -28,20 +34,28 @@ export default class PostsController {
     }
   }
 
-  async update(ctx: HttpContext) {
-    // TODO: Implement
-  }
-
   async show(ctx: HttpContext) {
     // TODO: Implement
   }
 
-  async destroy(ctx: HttpContext) {
+  async update({ response, bouncer, params }: HttpContext) {
+    const post = await Post.findOrFail(params.id)
+    if (await bouncer.with(PostPolicy).denies('edit', post)) {
+      return response.forbidden('Not the author of this post.')
+    }
     // TODO: Implement
   }
 
-   // TODO: Re-think of a better way to abstract this globally. https://vinejs.dev/docs/error_reporter
-   private errorsReducer(
+  async destroy({ params, bouncer, response }: HttpContext) {
+    const post = await Post.findOrFail(params.id)
+    if (await bouncer.with(PostPolicy).denies('edit', post)) {
+      return response.forbidden('Not the author of this post.')
+    }
+    // TODO: Implement
+  }
+
+  // TODO: Re-think of a better way to abstract this globally. https://vinejs.dev/docs/error_reporter
+  private errorsReducer(
     error: { field: string; message: string }[]
   ): Record<string, [{ message: string }]> {
     const reducedErrors = error.reduce((acc: any, cur: any) => {
