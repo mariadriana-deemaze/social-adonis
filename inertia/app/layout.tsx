6dfa4a7ac42236a539c9ee48605f26306edf0009
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2024-11-07 13:42:44 +0000

    fix: include favicon in bundle

--- inertia/app/layout.tsx
+++ inertia/app/layout.tsx
@@ -3,6 +3,7 @@ import { Toaster } from '@/components/ui/toaster'
 import { usePage } from '@inertiajs/react'
 import UserNavBar from '@/components/users/nav'
 import type { SharedProps } from '@adonisjs/inertia/types'
+import favicon from '../../public/assets/images/favicon.svg'
 
 export default function Layout({ children }: { children: ReactNode }) {
   const {
@@ -11,6 +12,7 @@ export default function Layout({ children }: { children: ReactNode }) {
 
   return (
     <>
+      <link rel="icon" type="image/svg+xml" href={favicon} />
       <UserNavBar user={user} />
       <div className="container flex justify-start pt-20">{children}</div>
       <Toaster />
