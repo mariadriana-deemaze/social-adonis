734ef873b6887ae47a32cc83f42a5ed004a2843d
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2024-11-08 18:21:06 +0000

    feat 🎸 (be): introduce other assets for upload

--- app/controllers/feed_controller.ts
+++ app/controllers/feed_controller.ts
@@ -2,6 +2,8 @@ import { inject } from '@adonisjs/core'
 import Post from '#models/post'
 import PostsService from '#services/posts_service'
 import { PostResponse } from 'app/interfaces/post'
+import { PageObject } from '@adonisjs/inertia/types'
+import { PaginatedResponse } from 'app/interfaces/pagination'
 import type { HttpContext } from '@adonisjs/core/http'
 
 @inject()
@@ -9,7 +11,7 @@ export default class FeedController {
   constructor(
     private readonly postsService: PostsService
   ) { }
-  async index(ctx: HttpContext) {
+  async index(ctx: HttpContext): Promise<string | PageObject<{ posts: PaginatedResponse<PostResponse> }>> {
     const page = ctx.request.qs().page || 1
 
     const posts = await Post.query()
