402184e4564239a115e377e8eabfa85f39311dd7
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2024-11-09 01:05:23 +0000

    feat 🎸 (be): add response interfaces

---
+++ app/interfaces/post.ts
@@ -0,0 +1,22 @@
+import { BaseEntity } from 'app/interfaces/base-entity'
+import { UUID } from 'crypto'
+import { UserResponse } from './user'
+
+export interface LinkMetadataJSONResponse {
+  title: string
+  description: string
+  thumbnail: string
+}
+
+export interface LinkResponse {
+  id: UUID
+  link: string
+  metadata: LinkMetadataJSONResponse
+}
+
+export interface PostResponse extends BaseEntity {
+  id: UUID
+  content: string
+  user: UserResponse
+  link: LinkResponse | null
+}
---
+++ app/interfaces/user.ts
@@ -0,0 +1,3 @@
+import { ModelObject } from '@adonisjs/lucid/types/model'
+
+export interface UserResponse extends ModelObject {} // TODO: Complete me.
