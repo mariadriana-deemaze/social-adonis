c989b5b22d67655f4a0845dd56be44df41447487
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2024-11-13 12:21:22 +0000

    fix ✅ (fe): correct cast

--- inertia/app/layout.tsx
+++ inertia/app/layout.tsx
@@ -4,13 +4,11 @@ import { usePage } from '@inertiajs/react'
 import UserNavBar from '@/components/users/nav'
 import type { SharedProps } from '@adonisjs/inertia/types'
 import favicon from '../../public/assets/images/favicon.svg'
-import { UserResponse } from '#interfaces/user'
 
 export default function Layout({ children }: { children: ReactNode }) {
-  const { props } = usePage<SharedProps>()
-
-  const user = props.user as UserResponse | null // FIX-ME: Bad practice. Will need to undestand how to lazy call services on the `./config/inertia.ts`
-
+  const {
+    props: { user },
+  } = usePage<SharedProps>()
   return (
     <>
       <link rel="icon" type="image/svg+xml" href={favicon} />
