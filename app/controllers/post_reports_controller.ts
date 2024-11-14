98b8d3bcd1c7559e2a62e6a871ee29efe657811a
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2024-11-14 11:51:56 +0000

    feat 🎸 (be): cont. on controller and services progress

--- app/controllers/post_reports_controller.ts
+++ app/controllers/post_reports_controller.ts
@@ -1,34 +1,79 @@
 import Post from '#models/post';
 import PostReport from '#models/post_report';
+import PostReportService from '#services/post_report_service';
+import { errorsReducer } from '#utils/index';
+import { postReportValidator } from '#validators/post_report';
+import { inject } from '@adonisjs/core';
+import { errors } from '@vinejs/vine';
 import type { HttpContext } from '@adonisjs/core/http'
 
+@inject()
 export default class PostReportsController {
 
- async create(ctx: HttpContext) {
-  const currentUser = ctx.auth.user?.id!
-  const postId = ctx.request.qs().id;
-  const post = await Post.findByOrFail(postId);
+  constructor(
+    private readonly service: PostReportService
+  ) { }
 
-  const payload = await ctx.request.body()
+  async create(ctx: HttpContext) {
+    const currentUserId = ctx.auth.user?.id!
+    const postId = ctx.request.qs().id;
+    const post = await Post.findByOrFail(postId);
 
-  if (await ctx.bouncer.with('PostReportPolicy').denies('create', post)) {
-   return ctx.response.forbidden('You cannot report your own post.')
+    if (await ctx.bouncer.with('PostReportPolicy').denies('create', post)) {
+      return ctx.response.forbidden('You cannot report your own post.')
+    }
+
+    try {
+      const payload = ctx.request.body()
+      const resource = this.service.create(currentUserId, post, payload)
+      return ctx.response.created(resource)
+    } catch (error) {
+      if (error instanceof errors.E_VALIDATION_ERROR) {
+        const reducedErrors = errorsReducer(error.messages)
+        return ctx.response.badRequest(reducedErrors);
+      }
+      return ctx.response.badRequest();
+    }
   }
 
-  /* 
-    const data = 
-  
-    PostReport.create({
-     reason
-    })
-   */
- }
+  async update(ctx: HttpContext) {
+    const postReportId = ctx.request.qs().id;
+    const postReport = await PostReport.findByOrFail(postReportId);
 
- async update(ctx: HttpContext) {
-  //
- }
+    if (await ctx.bouncer.with('PostReportPolicy').denies('edit', postReport)) {
+      return ctx.response.forbidden('You cannot update this report.')
+    }
 
- async destroy(ctx: HttpContext) {
-  //
- }
+    try {
+      const payload = ctx.request.body()
+      const resource = this.service.update(postReport, payload)
+      return ctx.response.ok(resource)
+    } catch (error) {
+      if (error instanceof errors.E_VALIDATION_ERROR) {
+        const reducedErrors = errorsReducer(error.messages)
+        return ctx.response.badRequest(reducedErrors);
+      }
+      return ctx.response.badRequest();
+    }
+  }
+
+  async destroy(ctx: HttpContext) {
+    const postReportId = ctx.request.qs().id;
+    const resource = await PostReport.findByOrFail(postReportId);
+
+    if (await ctx.bouncer.with('PostReportPolicy').denies('delete', resource)) {
+      return ctx.response.forbidden('You cannot delete this report.')
+    }
+
+    try {
+      await this.service.delete(resource)
+      return ctx.response.ok(resource)
+    } catch (error) {
+      if (error instanceof errors.E_VALIDATION_ERROR) {
+        const reducedErrors = errorsReducer(error.messages)
+        return ctx.response.badRequest(reducedErrors);
+      }
+      return ctx.response.badRequest();
+    }
+  }
 }
