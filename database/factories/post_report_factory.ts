3a05de8179a6f20b3fc97fcbc62b89af5cb64857
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2024-11-14 11:52:41 +0000

    test 🧪 (unit): add post report, and move parsing spec to correct spec folder

---
+++ database/factories/post_report_factory.ts
@@ -0,0 +1,21 @@
+import factory from '@adonisjs/lucid/factories'
+import PostReport from '#models/post_report'
+import { PostReportReason } from '#enums/post'
+import { UserFactory } from '#database/factories/user_factory'
+import { PostFactory } from '#database/factories/post_factory'
+import { randomUUID } from 'crypto'
+
+export const PostReportFactory = factory
+  .define(PostReport, async ({ faker }) => {
+    return {
+      reason: faker.helpers.enumValue(PostReportReason),
+      description: faker.lorem.paragraph(),
+      userId: randomUUID(),
+      postId: randomUUID()
+    }
+  })
+  .relation('user', () => UserFactory)
+  .relation('post', () => PostFactory)
+  .build()
+
+
---
+++ tests/unit/post_report_service.spec.ts
@@ -0,0 +1,98 @@
+import PostReport from '#models/post_report';
+import PostReportService from '#services/post_report_service'
+import { PostReportReason } from '#enums/post';
+import { UserFactory } from '#database/factories/user_factory';
+import testUtils from '@adonisjs/core/services/test_utils';
+import { faker } from '@faker-js/faker';
+import { test } from '@japa/runner'
+import { errors } from '@vinejs/vine';
+import { PostReportFactory } from '#database/factories/post_report_factory';
+
+test.group('Post report service', (group) => {
+
+  const service = new PostReportService();
+
+  const validPayload = {
+    reason: PostReportReason.ADULTERY,
+    description: faker.lorem.paragraph()
+  }
+
+  const invalidPayload = {
+    reason: faker.lorem.word(),
+    description: true
+  }
+
+  group.each.setup(() => testUtils.db().truncate())
+
+  test('Succesfully creates a new post report with valid payload', async ({ assert }) => {
+    const user = await UserFactory.with('posts', 1).create()
+
+
+    const result = await service.create(user.id, user.posts[0], validPayload)
+
+    assert.instanceOf(result, PostReport)
+    assert.containsSubset(result, validPayload)
+  })
+
+  test('Fails to create a new post report with invalid payload', async ({ assert }) => {
+    const user = await UserFactory.with('posts', 1).create()
+
+    let result: PostReport | typeof errors.E_VALIDATION_ERROR;
+    try {
+      result = await service.create(user.id, user.posts[0], invalidPayload)
+    } catch (error) {
+      result = error
+    }
+
+    assert.instanceOf(result, errors.E_VALIDATION_ERROR)
+    assert.containsSubset(JSON.parse(JSON.stringify(result)), {
+      messages: [
+        { message: "The selected reason is invalid", field: 'reason' },
+        { message: "The value of description field must be a string", field: 'description' }
+      ]
+    })
+  })
+
+  test('Succesfully updates a post report with valid payload', async ({ assert }) => {
+    const reportingUser = await UserFactory.create()
+    const user = await UserFactory.with('posts', 1, (post) => post.with('reports', 1, (report) => report.merge({
+      userId: reportingUser.id
+    }))).create()
+    const report = user.posts[0].reports[0];
+    const result = await service.update(report, validPayload)
+    assert.instanceOf(result, PostReport)
+    assert.containsSubset(result, validPayload)
+  })
+
+  test('Fails to update a post report with invalid payload', async ({ assert }) => {
+    const reportingUser = await UserFactory.create()
+    const user = await UserFactory.with('posts', 1, (post) => post.with('reports', 1, (report) => report.merge({
+      userId: reportingUser.id
+    }))).create()
+    const report = user.posts[0].reports[0];
+
+    let result: PostReport | typeof errors.E_VALIDATION_ERROR;
+    try {
+      result = await service.update(report, invalidPayload)
+    } catch (error) {
+      result = error
+    }
+    assert.instanceOf(result, errors.E_VALIDATION_ERROR)
+    assert.containsSubset(JSON.parse(JSON.stringify(result)), {
+      messages: [
+        { message: "The selected reason is invalid", field: 'reason' },
+        { message: "The value of description field must be a string", field: 'description' }
+      ]
+    })
+  })
+
+  test('Succesfully deletes a post report', async ({ assert }) => {
+    const reportingUser = await UserFactory.create()
+    const user = await UserFactory.with('posts', 1, (post) => post.with('reports', 1, (report) => report.merge({
+      userId: reportingUser.id
+    }))).create()
+    const report = user.posts[0].reports[0];
+    assert.doesNotThrow(() => service.delete(report))
+  })
+
+})
