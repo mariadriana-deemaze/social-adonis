9ea8298a62fccf940103377f21e1c2d811811176
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2024-11-17 02:05:01 +0000

    fix ✅: attempt to fix type def gen issue with explicit cmd run on build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import adonisjs from '@adonisjs/vite/client'
 import inertia from '@adonisjs/inertia/client'
 import react from '@vitejs/plugin-react'
 import { getDirname } from '@adonisjs/core/helpers'
+import { execSync } from 'node:child_process'
 
 export default defineConfig({
   plugins: [
@@ -13,6 +14,20 @@ export default defineConfig({
   resolve: {
     alias: {
       '@/': `${getDirname(import.meta.url)}/inertia/`,
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        {
+          name: 'generate-types',
+          buildStart() {
+            execSync('node ./node_modules/@izzyjs/route/build/src/generate_routes.js', {
+              stdio: 'inherit',
+            })
+          },
+        },
+      ],
     },
   },
 })
