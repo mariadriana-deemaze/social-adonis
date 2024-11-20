52ab4d920afe9d198e7df1f94a7013d8507872dc
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2024-11-20 16:30:59 +0000

    refactor ✨ : changed notification identifier type, and enable db pretty logs

--- database/migrations/1732106007391_create_notifications_table.ts
+++ database/migrations/1732106007391_create_notifications_table.ts
@@ -5,7 +5,7 @@ export default class extends BaseSchema {
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').primary()
+      table.uuid('id').primary().defaultTo(this.db.rawQuery('uuid_generate_v4()').knexQuery)
       table.json('data').notNullable()
       table.uuid('notifiable_id').unsigned().references('id').inTable('users').onDelete('CASCADE')
       table.timestamp('read_at', { useTz: false })
