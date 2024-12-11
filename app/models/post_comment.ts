e766a4b7a6bcf9fb1805aae536d15b5b0a8730ab
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2024-12-11 11:16:22 +0000

    feat 🎸 (be): add model and migration

---
+++ app/models/post_comment.ts
@@ -0,0 +1,41 @@
+import { DateTime } from 'luxon'
+import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
+import User from '#models/user'
+import Post from '#models/post'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import type { UUID } from 'node:crypto'
+
+export default class PostComment extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: UUID
+
+  @belongsTo(() => Post)
+  declare post: BelongsTo<typeof Post>
+
+  @column()
+  declare postId: UUID
+
+  @belongsTo(() => User)
+  declare user: BelongsTo<typeof User>
+
+  @column()
+  declare userId: UUID
+
+  @belongsTo(() => PostComment)
+  declare reply: BelongsTo<typeof PostComment>
+
+  @column()
+  declare replyId: UUID | null
+
+  @column()
+  declare content: string
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+
+  @column.dateTime()
+  declare deletedAt: DateTime
+}
---
+++ database/migrations/1733914761426_create_post_comments_table.ts
@@ -0,0 +1,22 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'post_comments'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').primary().defaultTo(this.db.rawQuery('uuid_generate_v4()').knexQuery)
+      table.uuid('post_id').references('posts.id').notNullable()
+      table.uuid('user_id').references('users.id').notNullable()
+      table.uuid('reply_id').references(this.tableName + '.id').nullable()
+      table.string('content')
+      table.timestamp('created_at', { useTz: false }).notNullable()
+      table.timestamp('updated_at', { useTz: false }).notNullable()
+      table.timestamp('deleted_at', { useTz: false }).notNullable()
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
