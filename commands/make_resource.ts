7fc2782348505ecffe0dffc400c1caac86430237
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2025-01-19 22:45:42 +0000

    chore 🏗️:: updated custom commands comments to be coherent

--- commands/make_resource.ts
+++ commands/make_resource.ts
@@ -1,30 +1,37 @@
 import { exec } from 'node:child_process'
 import { args, BaseCommand } from '@adonisjs/core/ace'
-import type { CommandOptions } from '@adonisjs/core/types/ace'
 
 export default class MakeResource extends BaseCommand {
+  /**
+   * Command Name is used to run the command
+   */
   static commandName = 'make:resource'
+
+  /**
+   * Command Name is displayed in the "help" output
+   */
   static description = 'Scaffold a project resource files'
 
+  /**
+   * Define the resource name parameter of the command
+   */
   @args.string({
     argumentName: 'resource',
     description: 'Name of the resources files to be created',
   })
   declare resource: string
 
-  static options: CommandOptions = {}
-
   async run() {
     const commands = [
-      `make:migration ${this.resource}`,
-      `make:model ${this.resource}`,
-      `make:service ${this.resource}`,
-      `make:controller ${this.resource}`,
-      `make:factory ${this.resource}`,
+      'make:migration',
+      'make:model',
+      'make:service',
+      'make:controller',
+      'make:factory',
     ]
 
     commands.forEach((command) => {
-      exec('node ace ' + command, (error, stdout, stderr) => {
+      exec(`node ace ${command} ${this.resource}`, (error, stdout, stderr) => {
         if (error) {
           this.logger.error(`error: ${error.message}`)
           return
